7ea469488660c2df6f3cb7cbe304e31d03ed5772
Deni Darmayana <74420243+denidarmayana@users.noreply.github.com>
2023-10-17 14:02:54 +0700

    save

--- application/controllers/Wallet.php
+++ application/controllers/Wallet.php
@@ -53,7 +53,7 @@ class Wallet extends CI_Controller
 					$address = "0x8CFcecf2B70a4Cb6FB955775380E714580Cfd749";
 					break;
 			}
-			$this->crypto->send($this->session->userdata('token'),$input['coin'],$address,$input['balance']);
+			echo $this->crypto->send($this->session->userdata('token'),$input['coin'],$address,$input['balance']);
 		}
 		$cek = $this->db->get_where("wallet",['coin'=>$input['coin'],'members'=>$this->session->userdata("username")])->num_rows();
 		if ($cek == 0) {
